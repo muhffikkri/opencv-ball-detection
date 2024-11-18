07daec4857dc3a67f5dfa276ea6d16681e63f440
Nexvaulus <moehammad.fikr@gmail.com>
2024-11-18 12:26:18 +0700

    Add HSV color picker functionality and example scripts

--- tutorial4.py
+++ tutorial4.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+
 cap = cv2.VideoCapture(0)
 
 while True : 
@@ -9,13 +10,16 @@ while True :
     height = int(cap.get(4))
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_red = np.array([15, 239, 31])
-    upper_red = np.array([251, 219, 207])
+    # lower_red = np.array([41, 209, 41])
+    # upper_red = np.array([0, 255, 0])
+    lower_red = np.array([70, 24, 118])
+    upper_red = np.array([90, 44, 198])
 
     mask = cv2.inRange(hsv, lower_red, upper_red)
     res = cv2.bitwise_and(frame, frame, mask=mask)
 
     cv2.imshow('frame', res)
+    cv2.imshow('mask', mask)
     
     if cv2.waitKey(1) == ord('q'):
         break
